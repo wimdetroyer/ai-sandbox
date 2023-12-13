c5297456001cf0487834024f5ce07e9c619e422c
wimdetroyer <wimdetroyer@gmail.com>
2023-12-13 17:26:05 +0000

    setup

---
+++ src/main/java/com/wdtr/aisandbox/AiSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.wdtr.aisandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wdtr/aisandbox/AiSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdtr.aisandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
